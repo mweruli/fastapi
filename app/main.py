e0a1a88bb9a0ff8963d8b22b73fc6ab601eda119
mweruli <mwerulijohnmoh@gmail.com>
2022-06-15 13:59:46 +0300

    second commit

--- app/main.py
+++ app/main.py
@@ -2,9 +2,10 @@ import imp
 from typing import Union
 
 from fastapi import FastAPI
-from app.routers import users
-from db.database import engine
-from db.models import user
+from routers import users
+from app.db.database import engine
+# from db.models import user
+from app.db.models import user
 
 user.Base.metadata.create_all(bind=engine)
 
